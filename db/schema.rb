3ab41ee57654ac498aa7fceae8aa89849b8ddd49
jvsa1102 <joaovalmeida1102@gmail.com>
2022-11-03 15:59:57 -0300

    Password type fix, simple user model validation

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_180821) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
