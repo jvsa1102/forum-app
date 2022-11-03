0e9158c59382630b7b7487e82559981a8388ce71
jvsa1102 <joaovalmeida1102@gmail.com>
2022-11-03 17:41:28 -0300

    Added homepage and model annotations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  root to: 'pages#home'
+  resources :users, only: :create
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
