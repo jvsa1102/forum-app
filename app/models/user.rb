9ad98b6cf0157d09621b1f5fd31481725ae7c2a5
jvsa1102 <joaovalmeida1102@gmail.com>
2022-11-04 16:26:01 -0300

    Added devise and bootsrap 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,6 @@
 class User < ApplicationRecord
-  validates :username, :email, :password, presence: true
-  validates :username, :email, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
-
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :bigint           not null, primary key
-#  username        :string
-#  password_digest :string
-#  email           :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
