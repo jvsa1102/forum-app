438757058c8b3c25c38e5980d07a5fadae34881a
jvsa1102 <joaovalmeida1102@gmail.com>
2022-11-04 13:52:02 -0300

    Small fix on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :users, only: :create
+  resources :users, only: [:create, :new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
